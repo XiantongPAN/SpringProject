2e1a9e3c7abdfae7d816d3fdfa1529960d018af3
Coston PAN <xiantongpan@gmail.com>
2018-02-09 17:35:21 +0800

    format

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
-
-
         HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
         helloWorld.sayHello();
     }
